791ef70ebdb0800fdeac1b831e9fcf114f41827f
Son Dinh <dinhs@objectcomputing.com>
2024-11-01 17:43:57 -0500

    Add data reader listeners

---
+++ tactical-microgrid-standard/controller/OperatorIntentRequestDataReaderListenerImpl.cpp
@@ -0,0 +1,6 @@
+#include "OperatorIntentRequestDataReaderListenerImpl.h"
+
+void OperatorIntentRequestDataReaderListenerImpl::on_data_available(DDS::DataReader_ptr reader)
+{
+  // TODO: Send the requested operation to the corresponding power device.
+}
